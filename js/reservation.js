aca4c2a483190af9e46f296e8a9b89f7181507c1
Alpha <alfa.2.walidjan@gmail.com>
2021-11-18 13:34:29 +0100

    mise en place du canva

--- js/reservation.js
+++ js/reservation.js
@@ -1,10 +1,36 @@
-export default class Reservation{
-    constructor(){
-
-
-        
-    }
+class Reservation {
+    constructor() {
+        this.formResa = document.querySelector('#form_velo');
+        this.mySubmit = document.querySelector('.submit');
 
  
 
+        this.myResaForm();
+
+
+       
+    }
+
+    myResaForm() {
+        this.formResa.addEventListener("submit", (e) => {
+
+            let canvasBloc = document.querySelector("#canvas");
+            let blocDescription = document.querySelector('#form_container');
+            let blocInfoStation = document.querySelector('#station_heading');
+
+            if (canvasBloc.style.display === "none") {
+
+                canvasBloc.style.display = "block";
+                blocInfoStation.style.display = "none";
+
+
+            }
+
+        })
+    }
 }
+
+
+
+
+const reservation = new Reservation();
